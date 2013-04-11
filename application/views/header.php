b19f360486e5363ff56da714f4682d6ad7312bbc
Jakub Dolejs <jakubdolejs@gmail.com>
2013-04-11 20:14:05 +0100

    Trade Gothic version

--- application/views/header.php
+++ application/views/header.php
@@ -2,48 +2,44 @@
 $this->lang->load("common");
 $languages = array("en"=>"English","fr"=>"Français");
 $lang = $this->config->item("language");
+$city = "";
+$nav_links = array();
+$path = parse_url($this->input->server("REQUEST_URI"), PHP_URL_PATH);
+$path_elements = explode("/", trim($path,"/ "));
+$other_city_url = "exhibitions";
 if (!empty($gallery_id)) {
-    $path = parse_url($this->input->server("REQUEST_URI"), PHP_URL_PATH);
-    $path_elements = explode("/", trim($path,"/ "));
-    $other_city_url = "exhibitions";
     if ($path_elements[0] == $gallery_id && count($path_elements) > 1) {
         $other_city_url = $path_elements[1];
     }
-}
-/*
-$lang_links = array();
-foreach ($languages as $lang_code=>$lang_name) {
-    if ($lang_code == $lang) {
-        $lang_links[] = $lang_name;
-    } else {
-        $getvars = $this->input->get(NULL,TRUE);
-        $getvars["language"] = $lang_code;
-        $lang_links[] = '<a href="'.$path.'?'.http_build_query($getvars).'">'.$lang_name.'</a>';
-    }
-}
-*/
-$city = "";
-$nav_links = array();
-if (!empty($gallery_id) && !empty($galleries)) {
-    $nav_links = array(
-        "/exhibitions"=>$this->lang->line("Exhibitions"),
-        "/artists"=>$this->lang->line("Artists"),
-        "/news"=>$this->lang->line("News"),
-        "/contact"=>$this->lang->line("Contact"),
-        "/about"=>$this->lang->line("About")
-    );
-    $gallery_nav_links = array();
-    foreach ($nav_links as $k=>$v) {
-        $gallery_nav_links["/".$gallery_id.$k] = $v;
-    }
-    foreach ($galleries as $gallery) {
-        if ($gallery["id"] == $gallery_id) {
-            $city = " ".$this->lang->line($gallery["city"]);
-        } else {
-            $gallery_nav_links["/".$gallery["id"]."/".$other_city_url] = $this->lang->line($gallery["city"]);
+    if (!empty($galleries)) {
+        $nav_links = array(
+            "/exhibitions"=>$this->lang->line("Exhibitions"),
+            "/artists"=>$this->lang->line("Artists"),
+            "/news"=>$this->lang->line("News"),
+            "/contact"=>$this->lang->line("Contact"),
+            "/about"=>$this->lang->line("About")
+        );
+        $gallery_nav_links = array();
+        foreach ($nav_links as $k=>$v) {
+            $gallery_nav_links["/".$gallery_id.$k] = $v;
+        }
+        foreach ($galleries as $gallery) {
+            if ($gallery["id"] == $gallery_id) {
+                $city = $this->lang->line($gallery["city"]);
+            } else {
+                $gallery_nav_links["/".$gallery["id"]."/".$other_city_url] = $this->lang->line($gallery["city"]);
+            }
+        }
+        $nav_links = $gallery_nav_links;
+        foreach ($languages as $lang_code=>$lang_name) {
+            if ($lang_code != $lang) {
+                $getvars = $this->input->get(NULL,TRUE);
+                $getvars["language"] = $lang_code;
+                $nav_links[$path.'?'.http_build_query($getvars)] = $lang_name;
+            }
         }
     }
-    $nav_links = $gallery_nav_links;
+} else {
     foreach ($languages as $lang_code=>$lang_name) {
         if ($lang_code != $lang) {
             $getvars = $this->input->get(NULL,TRUE);
@@ -65,17 +61,21 @@ if (!empty($gallery_id) && !empty($galleries)) {
     <body>
         <div id="content">
         <header>
-            <h1><a href="/"><?php echo $this->lang->line("Division Gallery").$city; ?></a></h1>
-            <?php if (!empty($nav_links)) { ?>
-            <a class="button" id="menuButton" href="javascript:void(0)">Menu</a>
             <nav>
                 <ul>
                     <?php
                     foreach ($nav_links as $url=>$title) {
-                        echo '<li><a href="'.$url.'">'.$title.'</a></li>';
+                        $class = "";
+                        if (!empty($gallery_id) && "/".$gallery_id."/".$other_city_url == $url) {
+                            $class = ' class="selected"';
+                        }
+                        echo '<li'.$class.'><a href="'.$url.'">'.$title.'</a></li>';
                     }
                     ?>
                 </ul>
             </nav>
+            <h1><a href="/"><?php echo $this->lang->line("Division Gallery"); ?> </a><?php echo $city; ?></h1>
+            <?php if (!empty($nav_links)) { ?>
+            <a class="button" id="menuButton" href="javascript:void(0)">Menu</a>
             <?php } ?>
         </header>
