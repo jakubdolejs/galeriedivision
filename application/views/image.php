2b18954c5fb5aac8e1c845171f2680764f48c694
Jakub Dolejs <jakubdolejs@gmail.com>
2013-08-08 16:05:50 +0100

    Use imperial inch notation on image dimensions.

--- application/views/image.php
+++ application/views/image.php
@@ -18,6 +18,20 @@ $title = $this->lang->line("Division Gallery");
             var lang = "<?php echo $lang; ?>";
             var imageId = <?php echo $image_id; ?>;
             var dragging = false;
+            function formatInches(number) {
+                var fraction = (Math.round(number*4)/4) % 1;
+                var num = Math.floor(number);
+                switch (fraction) {
+                    case 0.25:
+                        return num+" ¼";
+                    case 0.5:
+                        return num+" ½";
+                    case 0.75:
+                        return num+" ¾";
+                    default:
+                        return num;
+                }
+            }
             function loadImage(id) {
                 imageId = id;
                 if ($("div.slide img[src='/images/2mp/"+id+".jpg']").length > 0) {
@@ -76,14 +90,14 @@ $title = $this->lang->line("Division Gallery");
                             elements.push($('<span class="year"></span>').text(images[i].year));
                         }
                         var dimensions = [];
-                        if (images[i].hasOwnProperty("height")) {
-                            dimensions.push(images[i].height+'"');
+                        if (images[i].hasOwnProperty("height") && images[i].height) {
+                            dimensions.push(formatInches(images[i].height)+'"');
                         }
-                        if (images[i].hasOwnProperty("width")) {
-                            dimensions.push(images[i].width+'"');
+                        if (images[i].hasOwnProperty("width") && images[i].width) {
+                            dimensions.push(formatInches(images[i].width)+'"');
                         }
-                        if (images[i].hasOwnProperty("depth")) {
-                            dimensions.push(images[i].depth+'"');
+                        if (images[i].hasOwnProperty("depth") && images[i].depth) {
+                            dimensions.push(formatInches(images[i].depth)+'"');
                         }
                         if (dimensions.length > 0) {
                             dimensions = dimensions.join(" × ");
@@ -200,6 +214,20 @@ $title = $this->lang->line("Division Gallery");
 <body>
 <div class="imageViewer">
 <?php
+function format_inches($num) {
+    $number = floor($num);
+    $fraction = (round($num*4)/4) - $number;
+    switch ($fraction) {
+        case 0.25:
+            return $number." &frac14;";
+        case 0.5:
+            return $number." &frac12;";
+        case 0.75:
+            return $number." &frac34;";
+        default:
+            return $number;
+    }
+}
 if (!empty($images)) {
     if (empty($image_id)) {
         $image_id = $images[0]["id"];
@@ -238,13 +266,13 @@ if (!empty($images)) {
             }
             $dimensions = array();
             if (!empty($image["height"])) {
-                $dimensions[] = $image["height"].'&quot;';
+                $dimensions[] = format_inches($image["height"]).'&quot;';
             }
             if (!empty($image["width"])) {
-                $dimensions[] = $image["width"].'&quot;';
+                $dimensions[] = format_inches($image["width"]).'&quot;';
             }
             if (!empty($image["depth"])) {
-                $dimensions[] = $image["depth"].'&quot;';
+                $dimensions[] = format_inches($image["depth"]).'&quot;';
             }
             if (!empty($dimensions)) {
                 $info[] = '<span class="dimensions">'.join(" &times; ",$dimensions).'</span>';
