cccb5de131ebc8e970a808f633ccbb9b55af1bb5
Jakub Dolejs <jakubdolejs@gmail.com>
2013-10-04 10:34:47 +0100

    Updated Constant Contact access tokens

--- application/config/constant_contact.php
+++ application/config/constant_contact.php
@@ -4,7 +4,7 @@ if (ENVIRONMENT == "development") {
     $config["ctct_secret"] = "ZtdwjnmBud7RZYQt2CUpdCfA";
     $config["ctct_access_token"] = "f08c912b-bc5b-4773-a0a1-6075e5a93209";
 } else {
-    $config["ctct_api_key"] = "b3c45cdc-5283-4642-80ee-0ab7b31bdcc2";
-    $config["ctct_secret"] = "9372d9d150044b3585d12219844c0623";
-    $config["ctct_access_token"] = "4ff95b1c-f8f4-4ff5-a5a9-098fe88cf887";
+    $config["ctct_api_key"] = "qntanjx3n5d5x6uf33ws6929";
+    $config["ctct_secret"] = "NRhefH6YKMZZ9whRmBcNYtxj";
+    $config["ctct_access_token"] = "4d351a9c-098d-42b7-9fb1-587a6b1de59d";
 }
