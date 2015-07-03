e46f23079a1a951cfb03a3c4bce450f467a9c555
Jakub Dolejs <jakubdolejs@gmail.com>
2015-07-03 14:35:42 +0100

    Remove 'Also showing works by' title when section empty

--- application/views/artist.php
+++ application/views/artist.php
@@ -17,13 +17,15 @@ if (!empty($artists)) {
         }
         echo '<a class="artist" href="http://'.$this->input->server("HTTP_HOST").'/'.$gallery_id.'/artist/'.$artist["id"].'" itemprop="name">'.htmlspecialchars($artist["name"]).'</a></li>';
     }
-    echo '</ul><h1>'.$this->lang->line("Also showing works by").'</h1><ul class="thumbnails">';
-    foreach ($available as $artist) {
-        echo '<li itemscope itemtype="http://schema.org/Person">';
-        if (!empty($artist["galleries"][$gallery_id]["image_id"])) {
-            echo '<div><a href="http://'.$this->input->server("HTTP_HOST").'/'.$gallery_id.'/artist/'.$artist["id"].'" itemprop="url"><img itemprop="image" class="thumbnail" src="http://'.$this->input->server("HTTP_HOST").'/images/185/'.$artist["galleries"][$gallery_id]["image_id"].'.jpg" alt="image" /></a></div>';
+    if (!empty($available)) {
+        echo '</ul><h1>'.$this->lang->line("Also showing works by").'</h1><ul class="thumbnails">';
+        foreach ($available as $artist) {
+            echo '<li itemscope itemtype="http://schema.org/Person">';
+            if (!empty($artist["galleries"][$gallery_id]["image_id"])) {
+                echo '<div><a href="http://'.$this->input->server("HTTP_HOST").'/'.$gallery_id.'/artist/'.$artist["id"].'" itemprop="url"><img itemprop="image" class="thumbnail" src="http://'.$this->input->server("HTTP_HOST").'/images/185/'.$artist["galleries"][$gallery_id]["image_id"].'.jpg" alt="image" /></a></div>';
+            }
+            echo '<a class="artist" href="http://'.$this->input->server("HTTP_HOST").'/'.$gallery_id.'/artist/'.$artist["id"].'" itemprop="name">'.htmlspecialchars($artist["name"]).'</a></li>';
         }
-        echo '<a class="artist" href="http://'.$this->input->server("HTTP_HOST").'/'.$gallery_id.'/artist/'.$artist["id"].'" itemprop="name">'.htmlspecialchars($artist["name"]).'</a></li>';
+        echo '</ul>';
     }
-    echo '</ul>';
 }
