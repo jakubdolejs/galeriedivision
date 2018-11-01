79f16381e3d30b31e3f94fc4f3ba099eb571b2ed
Jakub Dolejs <jakubdolejs@gmail.com>
2018-11-01 20:55:29 +0000

    Changed http links to https

--- application/views/artist.php
+++ application/views/artist.php
@@ -13,18 +13,18 @@ if (!empty($artists)) {
     foreach ($represented as $artist) {
         echo '<li itemscope itemtype="http://schema.org/Person">';
         if (!empty($artist["galleries"][$gallery_id]["image_id"])) {
-            echo '<div><a href="http://'.$this->input->server("HTTP_HOST").'/'.$gallery_id.'/artist/'.$artist["id"].'" itemprop="url"><img itemprop="image" class="thumbnail" src="http://'.$this->input->server("HTTP_HOST").'/images/185/'.$artist["galleries"][$gallery_id]["image_id"].'.jpg" alt="image" /></a></div>';
+            echo '<div><a href="https://'.$this->input->server("HTTP_HOST").'/'.$gallery_id.'/artist/'.$artist["id"].'" itemprop="url"><img itemprop="image" class="thumbnail" src="https://'.$this->input->server("HTTP_HOST").'/images/185/'.$artist["galleries"][$gallery_id]["image_id"].'.jpg" alt="image" /></a></div>';
         }
-        echo '<a class="artist" href="http://'.$this->input->server("HTTP_HOST").'/'.$gallery_id.'/artist/'.$artist["id"].'" itemprop="name">'.htmlspecialchars($artist["name"]).'</a></li>';
+        echo '<a class="artist" href="https://'.$this->input->server("HTTP_HOST").'/'.$gallery_id.'/artist/'.$artist["id"].'" itemprop="name">'.htmlspecialchars($artist["name"]).'</a></li>';
     }
     if (!empty($available)) {
         echo '</ul><h1>'.$this->lang->line("Also showing works by").'</h1><ul class="thumbnails">';
         foreach ($available as $artist) {
             echo '<li itemscope itemtype="http://schema.org/Person">';
             if (!empty($artist["galleries"][$gallery_id]["image_id"])) {
-                echo '<div><a href="http://'.$this->input->server("HTTP_HOST").'/'.$gallery_id.'/artist/'.$artist["id"].'" itemprop="url"><img itemprop="image" class="thumbnail" src="http://'.$this->input->server("HTTP_HOST").'/images/185/'.$artist["galleries"][$gallery_id]["image_id"].'.jpg" alt="image" /></a></div>';
+                echo '<div><a href="https://'.$this->input->server("HTTP_HOST").'/'.$gallery_id.'/artist/'.$artist["id"].'" itemprop="url"><img itemprop="image" class="thumbnail" src="https://'.$this->input->server("HTTP_HOST").'/images/185/'.$artist["galleries"][$gallery_id]["image_id"].'.jpg" alt="image" /></a></div>';
             }
-            echo '<a class="artist" href="http://'.$this->input->server("HTTP_HOST").'/'.$gallery_id.'/artist/'.$artist["id"].'" itemprop="name">'.htmlspecialchars($artist["name"]).'</a></li>';
+            echo '<a class="artist" href="https://'.$this->input->server("HTTP_HOST").'/'.$gallery_id.'/artist/'.$artist["id"].'" itemprop="name">'.htmlspecialchars($artist["name"]).'</a></li>';
         }
         echo '</ul>';
     }
